2916966602ccbae74fbc1a41219ec74a534d1a87
SoNam11012 <sonamchoejur011@gmail.com>
2025-05-20 19:43:41 +0530

    Add ESLint ignore configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,39 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Base configurations
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+  // Global settings to ignore ESLint
+  {
+    ignores: [
+      // Add files or directories you want to completely ignore
+      '**/*.test.js',
+      'node_modules/**',
+      '.next/**',
+      'public/**',
+      // Add any other patterns you want to ignore
+    ],
+    rules: {
+      // Disable rules that are causing problems
+      'react/no-unescaped-entities': 'off',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/exhaustive-deps': 'warn', // Downgrade from error to warning
+      'no-unused-vars': 'warn', // Downgrade from error to warning
+      '@next/next/no-img-element': 'off',
+      // Add more rules to disable as needed
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        // Add globals that should be recognized
+        React: 'readonly',
+        process: 'readonly',
+      },
+    },
+  },
 ];
 
 export default eslintConfig;
